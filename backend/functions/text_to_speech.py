a265e48cca877ecb270b8ae86027273c64a92a99
MajidAlamoudi <78923018+MajidAlamoudi@users.noreply.github.com>
2023-11-13 14:56:02 +0400

    Update text_to_speech.py

--- backend/functions/text_to_speech.py
+++ backend/functions/text_to_speech.py
@@ -1,7 +1,7 @@
 import requests
 from decouple import config
 
-ELEVEN_LABS_API_KEY = config("111d5292eb4c9b66d2f4fc7fa211b56f", default="111d5292eb4c9b66d2f4fc7fa211b56f")
+ELEVEN_LABS_API_KEY = config("", default="")
 
 # Eleven labs
 # Convert text to speech
@@ -17,7 +17,7 @@ def convert_text_to_speech(message):
     }
 
     # Define voice
-    voice_rachel = "21m00Tcm4TlvDq8ikWAM"
+    voice_rachel = ""
     
     # Constructing headers and endpoint
     headers = {"xi-api-key": ELEVEN_LABS_API_KEY, "Content-Type": "application/json", "accept": "audio/mpeg"}
